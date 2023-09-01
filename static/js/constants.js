d8d586256212ed2d58b1ec21ef0a518d74f8e2e0
phlenfytest28@gmail.com <phlenfytest28@gmail.com>
2023-09-01 13:11:37 +0100

    just deployed

---
+++ static/js/constants.js
@@ -0,0 +1,5 @@
+const action_name = "action_hello_world";
+// var url = document.location.protocol + "//" + document.location.hostname;
+// const rasa_server_url = "http://7.tcp.eu.ngrok.io:16330/webhooks/rest/webhook";
+const rasa_server_url = "http://localhost:5005/webhooks/rest/webhook";
+const sender_id = uuidv4();
